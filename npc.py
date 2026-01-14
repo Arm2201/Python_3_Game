663892bc569e2c4ea9c05b34ce9a9fec46b15414
Arm2201 <armawesome22@gmail.com>
2026-01-14 13:30:57 +0700

    finish both classes

--- npc.py
+++ npc.py
@@ -1,1 +1,22 @@
 # Arm
+
+class NPC:
+    def __init__(self, x, y, vx, vy):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+
+    def update(self, x_min, x_max, y_min, y_max):
+        self.x += self.vx
+        self.y += self.vy
+
+        if self.x < x_min or self.x > x_max:
+            self.vx = -self.vx
+
+        if self.y < y_min or self.y > y_max:
+            self.vy = -self.vy
+
+        self.x = max(x_min, min(self.x, x_max))
+        self.y = max(y_min, min(self.y, y_max))
+
--- player.py
+++ player.py
@@ -1,1 +1,23 @@
 # Arm
+
+class Player:
+    def __init__(self, x, y, speed=1):
+        self.x = x
+        self.y = y
+        self.speed = speed
+
+    def move(self, keys, x_min, x_max, y_min, y_max):
+        if "a" in keys:
+            self.x -= self.speed
+        if "d" in keys:
+            self.x += self.speed
+        if "w" in keys:
+            self.y -= self.speed
+        if "s" in keys:
+            self.y += self.speed
+
+        # clamp to screen
+        self.x = max(x_min, min(self.x, x_max))
+        self.y = max(y_min, min(self.y, y_max))
+
+
